824014017d3f23ceba4c91766c5dfcd1e09f1ac7
Vishal Nathani <vasubhalodiya30@gmail.com>
2025-02-27 21:23:57 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,17 @@
+// replace below firebase config with your own
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWpvAzmfo1zJjtXuwCVS2vmeMG4sShCmQ",
+  authDomain: "netflix-lab.firebaseapp.com",
+  projectId: "netflix-lab",
+  storageBucket: "netflix-lab.firebasestorage.app",
+  messagingSenderId: "740885593825",
+  appId: "1:740885593825:web:c64ed9eb0c6c7defc701a0"
+};
+
+// craete a/c on themoviedb.org and replace access token below
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYzllYjZjNzI2NTczM2FhZDhiMTQ1NDBjYTRjZGY1ZiIsIm5iZiI6MTc0MDU4OTY5My42ODksInN1YiI6IjY3YmY0YTdkMTI5NDJlNTRkNzNkNTRlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0BC1SDmuMLwFbQ0I1Nmh3FI7ZF3GmHoQ6VthIn7zUyc";
+
+
+  export {firebaseConfig, TMDB_Access_Key};
